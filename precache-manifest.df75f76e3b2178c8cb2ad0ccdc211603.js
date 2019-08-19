9189132df9bf6cbd9c22f62d8ca1b22d80463ce6
Carolina Knoll <carolinasknoll@gmail.com>
2019-08-19 11:58:32 -0300

    Updates

---
+++ precache-manifest.df75f76e3b2178c8cb2ad0ccdc211603.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "902cd55648a34f5919690cf205fa1ede",
+    "url": "/wikipedia-viewer/index.html"
+  },
+  {
+    "revision": "4eac37a85cddcc291f9b",
+    "url": "/wikipedia-viewer/static/css/main.d2d79386.chunk.css"
+  },
+  {
+    "revision": "fdcba8101f08777a05db",
+    "url": "/wikipedia-viewer/static/js/2.acfedb50.chunk.js"
+  },
+  {
+    "revision": "4eac37a85cddcc291f9b",
+    "url": "/wikipedia-viewer/static/js/main.bb3f7ecf.chunk.js"
+  },
+  {
+    "revision": "2ddb60b92fe45b446e13",
+    "url": "/wikipedia-viewer/static/js/runtime~main.d5617d45.js"
+  },
+  {
+    "revision": "2ad1cda5cf7c4122df9a5686e5644f47",
+    "url": "/wikipedia-viewer/static/media/wikipedia-viewer-bg.2ad1cda5.jpg"
+  }
+]);
