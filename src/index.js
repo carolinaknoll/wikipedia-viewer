eee6e5a806d199463996bcd4162af1cb2c390f84
Carolina Knoll <carolinasknoll@gmail.com>
2019-08-19 11:33:00 -0300

    Remove react-router-dom dependency from index and package.json

--- src/index.js
+++ src/index.js
@@ -1,21 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {HashRouter, Route, Switch} from 'react-router-dom';
+import WikipediaViewer from '../src/containers/wikipedia-viewer';
 import registerServiceWorker from './registerServiceWorker';
-
 import './index.css';
 
-import WikipediaViewer from '../src/containers/wikipedia-viewer';
-
 ReactDOM.render(
-  <HashRouter>
-    <div>
-      <Route exact path='/' component={WikipediaViewer} />
-      <Switch>
-        <Route path='/wikipedia-viewer' components={WikipediaViewer} />
-      </Switch>
-    </div>
-  </HashRouter>,
+  <WikipediaViewer/>,
   document.getElementById('root'),
 );
 
